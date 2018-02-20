94f51ad53eee466825825471a3996a349baf09a1
bebee <253824369@qq.com>
2018-02-20 20:49:32 +0800

    提交隐式转换测试

---
+++ test/src/org/scala/test/ImplicitTest.scala
@@ -0,0 +1,48 @@
+package org.scala.test
+
+/**
+  * Created by sgr on 2018/2/20/020.
+  */
+
+class SpecialPerson(val name:String){
+  def speak() = {
+    println("i'm special person!call me "+ name)
+  }
+}
+class Older(val name:String)
+class Student(val name:String)
+class Teacher(val name:String)
+
+object ImplicitTest {
+
+  //隐式转换
+  implicit def person2Special(obj: Object) : SpecialPerson = {
+    if (obj.getClass == classOf[Older]){
+      val older = obj.asInstanceOf[Older]
+      new SpecialPerson(older.name)
+    }else if (obj.getClass == classOf[Student]){
+      val student = obj.asInstanceOf[Student]
+      new SpecialPerson(student.name)
+    }else{
+      println("Nil")
+      Nil
+    }
+  }
+
+  var sumTicket = 0
+
+  def buySpecialTicket(specialPerson: SpecialPerson) = {
+    sumTicket += 1
+    println(sumTicket)
+  }
+
+  def main(args: Array[String]) {
+    val older = new Older("老王")
+    older.speak()
+    buySpecialTicket(older)
+    val teacher = new Teacher("王老师")
+    buySpecialTicket(teacher)
+
+  }
+
+}
